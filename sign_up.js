4fe876010d00054b32218316accaef5df5c2bc81
Present-truth <presenttrouble@gmail.com>
2025-05-28 02:49:25 +0100

    Add files via upload

---
+++ sign_up.js
@@ -0,0 +1,6 @@
+// This is for themonth display
+
+function OneContainer() {
+    var OneContainer = document.querySelector(".play-img-one");
+    OneContainer.style.display = OneContainer.style.display === "block" ? "none" : "block";
+}
